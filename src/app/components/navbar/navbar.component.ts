c4d431109969cdd0773f212c1424fdf07dbada90
julianolanzi <julianolanzi@hotmail.com>
2024-07-07 12:27:05 -0300

    ajuste de imgem e notificações na navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { url } from '../../state-management/selectors/global-pages.selector';
+import { GlobalState } from '../../state-management/states/global.state';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  url$: Observable<string> = this.store.pipe(select(url));
+  constructor(private store: Store<GlobalState>) {
 
+  }
 }
